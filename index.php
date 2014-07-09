832527fd0927d91be76f3ef188d9529064628c33
Vitthal Awate <v.awate@easternenterprise.com>
2014-07-09 14:18:01 +0530

    index.php adding

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+	$link = mysql_connect("localhost", "root", "root") or die(mysql_error());
+	mysql_select_db("test") or die(mysql_error());
+	$query = mysql_query("select * from tblTest") or die(mysql_error());
+	while($info = mysql_fetch_array($query)) {
+		echo "<br>" . $info['name'];	
+	}
+?>
